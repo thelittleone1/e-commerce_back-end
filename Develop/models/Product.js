0fb24f0b4a5a296a7227b4790cfc24d685ad0628
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-19 22:56:06 -0700

    updating category model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,27 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // Add a validate
+      },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      
+    }
+
   },
   {
     sequelize,
