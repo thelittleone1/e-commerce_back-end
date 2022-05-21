323faeb7539fe31da1022345c955310f9d75b933
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-05-21 11:13:35 -0700

    updating .env

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-
+//require('dotenv').config();
+require('dotenv').config({path:__dirname+'/./../.env'})
 const Sequelize = require('sequelize');
 
 console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW);
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize("ecommerce_db", "root", "password", {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
